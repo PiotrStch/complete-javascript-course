b00f0618712a1c076ff89937ef9034a6e2e7fc13
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-09-04 21:06:04 +0200

    end of lesson 12.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,3 +20,33 @@ let myFirstJob = 'programmer'
 let myCurrentJob = 'teacher' //these are more expressive name of variables
 
 console.log(myFirstJob);
+
+// 7 primitive DATA TYPES
+// 1. number
+// 2. STRINGS
+// 3. BOOLEAN
+
+// 4. UNDEFINED - not yet defined (empty values)
+// 5. NULL
+// 6. SYMBOL (ES2015) values that is unique and cannot be ChannelMergerNode
+// 7. BigInt (ES2020) larger integers than the Number type can hold
+
+// dynamic typing = automatically determined data types!!!
+let jsIsFun = true
+console.log(jsIsFun);
+
+console.log(typeof jsIsFun);
+console.log(typeof false);
+console.log(typeof 'mamma');
+console.log(typeof null);
+
+jsIsFun = 'YES!'
+console.log(jsIsFun);
+console.log(typeof jsIsFun);
+
+let year;
+console.log(year);
+
+year = 1985;
+console.log(typeof year);
+
