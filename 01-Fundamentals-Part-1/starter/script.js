90cc0ffa35be63178988ab406a522e9361d1ba15
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-09-04 19:42:34 +0200

    end of lesson 10

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,22 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
 console.log(4 + 12 + 83 + 5);
+
+console.log('Jonas');
+console.log(45);
+
+let firstName = 'Jonas' //declaring value of variable
+let first = 'jonas'
+let firstNamePerson //camelCase in JS
+let first_name_ //f.e. in Ruby programming language
+
+console.log(firstName);
+
+/*  //don't name this way//
+let 3 = hsdg //number as a name of variables
+let new = 36 //reserved key words (function, $ & only"name", start with upperCase letter)
+*/
+
+let myFirstJob = 'programmer'
+let myCurrentJob = 'teacher' //these are more expressive name of variables
+
+console.log(myFirstJob);
