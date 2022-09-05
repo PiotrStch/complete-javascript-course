6a09387aa3875a3d3064e1762cdb14a911e4e4b2
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-09-05 16:18:10 +0200

    end of 32 lesson

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,12 @@
-"use strict";
+"use strict"; //pomaga znaleźć błędy
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("Ican drive");
+
+
+// const interface = 'Audio'
+// const private = 8367
+// const 6dj8 = 78
