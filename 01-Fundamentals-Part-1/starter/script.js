65662880ce6144ac95b865273f1aa3fddf396cda
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-09-05 10:21:06 +0200

    lesson 19

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -81,8 +81,8 @@ const massJohn = 92;
 const heightMark = 1.69;
 const heightJohn = 1.95;
 
-const bmiMark = massMark / heightMark ** 2;
-const bmiJohn = massJohn / heightJohn ** 2;
+const bmiMark = (massMark / heightMark ** 2).toPrecision(4);
+const bmiJohn = (massJohn / heightJohn ** 2).toPrecision(4);
 console.log(bmiMark, bmiJohn);
 
 const markHigherBMI = bmiMark > bmiJohn;
@@ -109,3 +109,24 @@ lines');
 console.log(`String
 multiple
 lines`);
+
+const age = 17
+
+if (age >= 18) {
+    console.log('you can drive a car');
+}
+else {
+    console.log('you are too young');
+}
+
+/*lesson 19Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message 
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"*/
+
+if (bmiMark > bmiJohn) {
+console.log(`Mark's BMI ${bmiMark} is higher than John's BMI ${bmiJohn}!`)}
+else {
+    console.log(`John's BMI ${bmiJohn} is higher than Mark's BMI ${bmiMark}!`)  
+}
