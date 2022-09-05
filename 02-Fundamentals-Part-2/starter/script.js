5e6331832c5fdf8ba7e92288c21c09f3503d7fdd
PiotrStch <105080464+PiotrStch@users.noreply.github.com>
2022-09-05 16:40:37 +0200

    end of 33 lesson

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,7 +6,26 @@ const passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("Ican drive");
 
-
 // const interface = 'Audio'
 // const private = 8367
 // const 6dj8 = 78
+
+// FUNCTIONS
+
+function logger() {
+	console.log("ciało funkcji");
+}
+
+// calling, invoking, running function
+logger();
+
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(4, 0);
+console.log(appleJuice);
+
+const num4 = Number("36")
